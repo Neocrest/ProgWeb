55f141a1add8a84c6b0b31c61e2babcb5980fe85
Neocrest <35774506+Neocrest@users.noreply.github.com>
2019-02-12 17:43:47 +0100

    Animation player selon spritesheet //le pausecomp du drawPlayer doit avoir un paramètre assez bas pour limiter la latence

--- TP3/js/game.js
+++ TP3/js/game.js
@@ -31,9 +31,9 @@ var keys = {
 var keyStatus = {};
 
 function keyDownHandler(event) {
-    "use strict"; 
-    var keycode = event.keyCode, 
-        key; 
+    "use strict";
+    var keycode = event.keyCode,
+        key;
     for (key in keys) {
         if (keys[key] === keycode) {
             keyStatus[keycode] = true;
@@ -44,11 +44,11 @@ function keyDownHandler(event) {
 function keyUpHandler(event) {
    var keycode = event.keyCode,
             key;
-    for (key in keys) 
+    for (key in keys)
         if (keys[key] == keycode) {
             keyStatus[keycode] = false;
         }
-        
+
     }
 ///////////////////////////////////
 
@@ -57,7 +57,7 @@ function keyUpHandler(event) {
 /////////////////////////////////
 // Hero Player
 var imgPlayer = new Image();
-imgPlayer.src = "./assets/Ship/f1.png";
+imgPlayer.src = "./assets/Ship/Spritesheet_64x29.png";
 var xPlayer = 20;
 var yPlayerSpeed = 10;
 var yPlayer = 100;
@@ -65,72 +65,86 @@ var PlayerHeight = 15;
 var PlayerWidth = 32;
 var PlayerImgHeight = 29;
 var PlayerImgWidth = 64;
+var i=0;
 /////////////////////////////////
 
 
 
 function updateScene() {
-    "use strict"; 
+    "use strict";
     xBackgroundOffset = (xBackgroundOffset - xBackgroundSpeed) % backgroundWidth;
 }
 function updateItems() {
-    "use strict"; 
+    "use strict";
     clearItems();
-    
+
     var keycode;
     for (keycode in keyStatus) {
             if(keyStatus[keycode] == true){
-                if(keycode == keys.UP) { 
-                    yPlayer -= yPlayerSpeed;   
+                if(keycode == keys.UP) {
+                    yPlayer -= yPlayerSpeed;
                 }
-                if(keycode == keys.DOWN) { 
-                    yPlayer += yPlayerSpeed;   
-                } 
-                if(keycode == keys.Space) { 
+                if(keycode == keys.DOWN) {
+                    yPlayer += yPlayerSpeed;
+                }
+                if(keycode == keys.Space) {
                     //shoot
-                }             
+                }
             }
         keyStatus[keycode] = false;
     }
 }
 function drawScene() {
-    "use strict"; 
+    "use strict";
     canArena.style.backgroundPosition = xBackgroundOffset + "px 0px" ;
 }
+function drawPlayer() {
+  "use strict";
+      if(i>3){
+          i=0;
+      }
+        conArena.clearRect(xPlayer,yPlayer,PlayerWidth,PlayerHeight);
+        conArena.drawImage(imgPlayer, 0, PlayerImgHeight*i, PlayerImgWidth, PlayerImgHeight, xPlayer, yPlayer, PlayerWidth, PlayerHeight);
+        i++;
+      requestAnimationFrame(drawPlayer);
+      //si millis dépasse 5, la latence se fait ressentir sur le déplacement du joueur (problème de sync)
+      pausecomp(3);
+  }
 function drawItems() {
-    "use strict"; 
+    "use strict";
     conArena.drawImage(imgPlayer, 0,0,PlayerImgWidth,PlayerImgHeight, xPlayer,yPlayer,PlayerWidth,PlayerHeight);
 }
 function clearItems() {
-    "use strict"; 
+    "use strict";
     conArena.clearRect(xPlayer,yPlayer,PlayerWidth,PlayerHeight);
 }
 
 function updateGame() {
-    "use strict"; 
+    "use strict";
     updateScene();
     updateItems();
 }
 
 function drawGame() {
-    "use strict"; 
+    "use strict";
     drawScene();
-    drawItems();    
+    drawPlayer();
+    drawItems();
 }
 
 
 function mainloop () {
-    "use strict"; 
+    "use strict";
     updateGame();
     drawGame();
 }
 
 function recursiveAnim () {
-    "use strict"; 
+    "use strict";
     mainloop();
     animFrame( recursiveAnim );
 }
- 
+
 function init() {
     "use strict";
     divArena = document.getElementById("arena");
@@ -138,13 +152,20 @@ function init() {
     canArena.setAttribute("id", "canArena");
     conArena = canArena.getContext("2d");
     divArena.appendChild(canArena);
- 
-    
+
+
 window.addEventListener("keydown", keyDownHandler, false);
 window.addEventListener("keyup", keyUpHandler, false);
-    
+
     animFrame( recursiveAnim );
-    
+
 }
 
 window.addEventListener("load", init, false);
+
+
+function pausecomp(millis)  {
+        var date = new Date();
+        var curDate = null;
+        do {            curDate = new Date();} while(curDate-date < millis);
+    }
